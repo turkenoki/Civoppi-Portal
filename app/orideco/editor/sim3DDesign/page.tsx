688844da19a7197c93f824109ec749c86f995be3
turkenoki <turkenoki@gmail.com>
2025-06-19 12:38:50 +0900

    Open preview in new page

---
+++ app/orideco/editor/sim3DDesign/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import TshirtsWithDecal from "@/components/orideco/TshirtsWithDecal";
+
+export default function Sim3DDesign() {
+  return (
+    <main>
+      <TshirtsWithDecal />
+    </main>
+  );
+}
